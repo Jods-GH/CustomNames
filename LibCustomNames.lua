8c62a1d2fb24403166b9d52bedf563081a04c7d9
Jods <39654549+Jodsderechte@users.noreply.github.com>
2023-05-30 11:32:59 +0200

    added Unit functionality and UnitName(unit), UnitFullName(unit), GetUnitName(unit,showServerName) options

--- LibCustomNames.lua
+++ LibCustomNames.lua
@@ -1,5 +1,5 @@
-local MAJOR_VERSION = "LibCustomNames-1.0"
-local MINOR_VERSION = 2
+local MAJOR_VERSION = "LibCustomNames"
+local MINOR_VERSION = 3
 if not LibStub then error(MAJOR_VERSION .. " requires LibStub.") end
 local lib = LibStub:NewLibrary(MAJOR_VERSION, MINOR_VERSION)
 if not lib then error("LibCustomNames failed to initialise")return end
@@ -17,18 +17,103 @@ end
 
 function lib:Set(name, customName)
     assert(name, "LibCustomNames: Can't SetCustomName (name is nil)")
-    assert(name:match("^(.+)-(.+)$"), "LibCustomNames: Can't set custom Name (name is not in the correct format Name-Realm)")
-    if not customName then
-        LibCustomNamesDB[name] = nil
-        return true
-    else
-        LibCustomNamesDB[name] = customName
-        return true
-    end
+	if UnitExists(name) then	
+		local unitName, unitRealm = UnitName(name)
+		if UnitIsPlayer(name) then
+			name = unitName .. "-" .. (unitRealm or GetRealmName())
+		else
+			name = unitName
+		end
+	else
+		assert(name:match("^(.+)-(.+)$"), "LibCustomNames: Can't set custom Name (name is not in the correct format Name-Realm)")
+	end
+	if not customName then
+		LibCustomNamesDB[name] = nil
+		return true
+	else
+		LibCustomNamesDB[name] = customName
+		return true
+	end
 end
 
 function lib:GetList()
     return CopyTable(LibCustomNamesDB)
+end
+
+function lib:UnitName(unit)
+	assert(unit, "LibCustomNames: Can't GetUnitName (unit is nil)")
+	assert(UnitExists(unit), "LibCustomNames: Can't GetUnitName (unit does not exist)")
+	local unitName, unitRealm = UnitName(unit)
+	local nameToCheck = unitName .. "-" .. (unitRealm or GetRealmName())
+	local customName = lib:Get(nameToCheck)
+	if customName ~= nameToCheck then
+		return customName,unitRealm
+	else
+		return unitName,unitRealm
+	end
+end
+
+function lib:UnitFullName(unit)
+	assert(unit, "LibCustomNames: Can't GetUnitName (unit is nil)")
+	assert(UnitExists(unit), "LibCustomNames: Can't GetUnitName (unit does not exist)")
+	local unitName, unitRealm = UnitFullName(unit)
+	local nameToCheck
+	if UnitIsPlayer(unit) then
+		nameToCheck = unitName .. "-" .. (unitRealm or GetRealmName())
+	else
+		nameToCheck= unitName
+	end
+	local customName = lib:Get(nameToCheck)
+	if customName ~= nameToCheck then
+		return customName,unitRealm
+	else
+		return unitName,unitRealm
+	end
+end
+
+function lib:GetUnitName(unit,showServerName)
+	assert(unit, "LibCustomNames: Can't GetUnitName (unit is nil)")
+	assert(UnitExists(unit), "LibCustomNames: Can't GetUnitName (unit does not exist)")
+	local unitName, unitRealm = UnitFullName(unit)	
+	local nameToCheck
+	if UnitIsPlayer(unit) then
+		nameToCheck = unitName .. "-" .. (unitRealm or GetRealmName())
+	else
+		nameToCheck= unitName
+	end
+	local customName = lib:Get(nameToCheck)
+	local realmRelationship = UnitRealmRelationship(unit)
+	if realmRelationship == LE_REALM_RELATION_SAME or not realmRelationship then
+		if customName ~= nameToCheck then
+			return customName
+		else
+			return unitName
+		end
+	elseif realmRelationship == LE_REALM_RELATION_VIRTUAL then
+		if customName ~= nameToCheck then
+			if showServerName then
+				return customName .. "-" .. unitRealm
+			else
+				return customName
+			end
+		else
+			return unitName .. "-" .. unitRealm
+		end
+	else --LE_REALM_RELATION_COALESCED 
+		if customName ~= nameToCheck then
+			if showServerName then
+				return customName .. "-" .. unitRealm
+			else
+				return customName.." (*)"
+			end
+		else 
+			if showServerName then
+				return unitName .. "-" .. unitRealm
+			else
+				return unitName.." (*)"
+			end
+		end
+	end
 end
 
 SLASH_LibCustomNames1 = '/LCN'
--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 Adds Management of Custom Names
 # Usage 
 ```lua
-local LibCustomNames= LibStub("LibCustomNames-1.0")
+local LibCustomNames= LibStub("LibCustomNames")
 local customName = LibCustomNames:Get(name)
-local success = LibCustomNames:Set(name, customName) -- for adding/editing
-local success = LibCustomNames:Set(name) -- for deleting
+local success = LibCustomNames:Set(name, customName) -- for adding/editing accepts units aswell as Names in the format of Name-Realm (for players) or just Name (for npcs)
+local success = LibCustomNames:Set(name) -- for deleting ccepts units aswell as Names in the format of Name-Realm (for players) or just Name (for npcs)
 local NameList = LibCustomNames:GetList()
+local customName = lib:UnitName(unit) -- behaves equivalent to normal UnitName()
+local customName = lib:UnitFullName(unit) -- behaves equivalent to normal UnitFullName()
+local customName = lib:GetUnitName(unit,showServerName) -- behaves equivalent to normal GetUnitName()
 ```
